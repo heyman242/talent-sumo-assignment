c46f1c176bbc2166c056f2f107eb58f04472a1f4
Himanshu Bobade <himanshubobade96@gmail.com>
2023-06-21 15:03:39 +0530

    created database tables

--- notes_app/notes/models.py
+++ notes_app/notes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Note(models.Model):
+    TYPE_CHOICES = (
+        ('text', 'Text'),
+        ('audio', 'Audio'),
+        ('video', 'Video'),
+    )
+
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    shared_with = models.ManyToManyField(User, related_name='shared_notes', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
--- notes_app/notes/urls.py
+++ notes_app/notes/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from .views import NoteListAPIView
 
 urlpatterns =[  
+    path('notes/', NoteListAPIView.as_view(), name='note-list'),
 ]
